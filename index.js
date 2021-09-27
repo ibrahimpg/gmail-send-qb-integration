9f525c18c71a3c76b04d76e7a91de1747fb910d4
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2021-09-27 20:29:29 +0300

    revert back to POST

--- index.js
+++ index.js
@@ -4,17 +4,14 @@ const gmailCall = require('./nodemailer');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded());
 
-app.get('/send/:serverPw/:to/:subject/:text', async (req, res) => {
+app.post('/send', async (req, res) => {
   try {
     const {
       serverPw, to, subject, text,
-    } = req.params;
+    } = req.body;
 
     const validTo = to.slice(1, to.length).replace(';', ',');
-
-    console.log(validTo);
 
     if (serverPw !== process.env.SERVER_PASS) return res.sendStatus(401);
 
